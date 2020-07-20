f4733c48d5b6e1d3af30b1d449e23e927d36ff17
Amber <amber.jones@usu.edu>
2020-07-20 14:26:15 -0600

    functionalizing the LSTM code.

--- LSTM_explore.py
+++ LSTM_explore.py
@@ -8,7 +8,9 @@ from matplotlib.pylab import rcParams
 import matplotlib.pyplot as plt
 import plotly.express as px
 import plotly.graph_objects as go
-
+from sklearn.preprocessing import StandardScaler
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, LSTM, Dropout, RepeatVector, TimeDistributed
 import plotly.io as pio
 pio.renderers.default = "browser"
 
@@ -19,63 +21,106 @@ tf.random.set_seed(1)
 
 print('Tensorflow version:', tf.__version__)
 
-## Task 2: Load and Inspect Data
+# Import Data
 
-df = pd.read_csv('/Users/amber/PycharmProjects/LRO-anomaly-detection/LRO_data/MainStreet2014.csv', parse_dates=['datetime'])
+### DEFINE SITE and VARIABLE ###
+# Inputs: Raw Data, Corrected Data, Parameters (p,d,q, threshold)
+# declarations: site, model parameters (threshold and (p, d, q))
+# site = "BlackSmithFork"
+# site = "FranklinBasin"
+# site = "MainStreet"
+site = "Mendon"
+# site = "TonyGrove"
+# site = "WaterLab"
+# sensor = "temp"
+sensor = "cond"
+# sensor = "ph"
+# sensor = "do"
+# sensor = "turb"
+# sensor = "stage"
+year = 2017
 
-df = df[['datetime','cond']]
+
+def get_data(site, sensor, year, path=""):
+    """Imports a single year of data based on files named by site, sensor/variable, and year.
+    Labels data as anomalous. Generates a series from the data frame."""
+    # TODO: make sensors input argument a list and output df with multiple normal_lbl columns.
+    if path == "":
+        path = os.getcwd()
+    df_full = pd.read_csv(path + site + str(year) + ".csv",
+                     skipinitialspace=True,
+                     engine='python',
+                     header=0,
+                     index_col=0,
+                     parse_dates=True,
+                     infer_datetime_format=True)
+    # makes one-dimensional data frame of booleans based on qualifier column indicating normal (TRUE) or not (FALSE)
+    normal_lbl = df_full[sensor + "_qual"].isnull()
+    # generate series from dataframe - time indexed values
+    df = df_full[[sensor]]
+    srs = pd.Series(df_full[sensor])
+
+    return df_full, df, normal_lbl, srs
+
+
+df_full, df, normal_lbl, srs = get_data(site, sensor, year, path="/Users/amber/PycharmProjects/LRO-anomaly-detection/LRO_Data/")
 df.head()
-
 df.shape
 
+# Inspect data
 # plotting with plotly to have an interactive chart
 fig = go.Figure()
-fig.add_trace(go.Scatter(x=df.datetime, y=df.cond, #fig.add_trace adds different types of plots to the same figure.
+# fig.add_trace(go.Scatter(x=df.datetime, y=df.cond, #fig.add_trace adds different types of plots to the same figure.
+fig.add_trace(go.Scatter(x=df.index, y=df.cond, #fig.add_trace adds different types of plots to the same figure.
                     mode='lines',
                     name='cond'))
 fig.update_layout(showlegend=True)
 fig.show()
 
-## Task 3: Data Preprocessing
-
-# create training and testing set. Use 80/20.
-train_size = int(len(df) * 0.8)
-test_size = len(df) - train_size
-train, test = df.iloc[0:train_size], df.iloc[train_size:len(df)]
-print(train.shape, test.shape)
-
-# remove mean and scale to unit vectors with standard scalar.
-
-from sklearn.preprocessing import StandardScaler
-
-scaler = StandardScaler()
-scaler = scaler.fit(train[['cond']])
-
-train['cond'] = scaler.transform(train[['cond']])
-test['cond'] = scaler.transform(test[['cond']])
+# Data Preprocessing
 
 
-## Task 4: Create Training and Test Splits
+def train_test(df, ratio):
+    """splits data fram into training and testing. uses straight temporal split."""
+    # TODO: Add ability to do randomized train/test
+    train_size = int(len(df) * ratio)
+    test_size = len(df) - train_size
+    train, test = df.iloc[0:train_size], df.iloc[train_size:len(df)]
 
+    return train, test
 
-# need to reshape data to "temporalize" into number of samples/timestamps by number of features.
-# We have one feature, we will set samples size to 200 (30) for sequences. We will do this many times, so create a function.
-# time steps is the number of rows to consider. Start with 1 and then will change to 200 (30).
+train, test = train_test(df, 0.8)
+
+def scale_data(train, test):
+    """scales data using standard scaler. removes mean and scales to unit vectors."""
+    scaler = StandardScaler()
+    scaler = scaler.fit(train)
+    train[sensor] = scaler.transform(train)
+    test[sensor] = scaler.transform(test)
+
+    return train, test
+
+train, test = scale_data(train, test)
+
 def create_dataset(X, y, time_steps=1):
+    """Reshapes data to temporalize it into (samples, timestamps, features).
+    Time stamps defines a sequence of how far back to consider for each sample/row.
+    Features refers to the number of columns/variables."""
     Xs, ys = [], [] # start empty list
-    for i in range(len(X) - time_steps): # loop within range of data frame minus the time steps
-        v = X.iloc[i:(i + time_steps)].values # data from i to end of the time step
+    for i in range(len(X) - time_steps):  # loop within range of data frame minus the time steps
+        v = X.iloc[i:(i + time_steps)].values  # data from i to end of the time step
         Xs.append(v)
         ys.append(y.iloc[i + time_steps])
-    return np.array(Xs), np.array(ys) # convert lists into numpy arrays and return
+
+    return np.array(Xs), np.array(ys)  # convert lists into numpy arrays and return
 
 # create X and y train and test
 # set time steps to 200(30)
 
 time_steps = 30
 
-X_train, y_train = create_dataset(train[['cond']], train.cond, time_steps)
-X_test, y_test = create_dataset(test[['cond']], test.cond, time_steps)
+X_train, y_train = create_dataset(train, train, time_steps)
+X_test, y_test = create_dataset(test, test, time_steps)
 
 print(X_train.shape)
 
@@ -87,26 +132,35 @@ print(X_train.shape)
 timesteps = X_train.shape[1]
 num_features = X_train.shape[2]
 
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense, LSTM, Dropout, RepeatVector, TimeDistributed
+
 
 # use sequential model class from keras api - can provide definition as list of layers to the sequential data class.
 # input to data - sample is one instance from the dataset (row). number of timesteps by number of features.
 # output is the encoded feature vector of the input data
 # can change hyperparams (number cells, dropout rate)
 
-model = Sequential([
-    LSTM(128, input_shape=(timesteps, num_features)),  # one LSTM layer
-    Dropout(0.2),  # dropout regularization
-    RepeatVector(timesteps),  # replicates the feature vectors from LSTM layer output vector by the number of time steps (e.g., 200(30) times)
-    LSTM(128, return_sequences=True),  # mirror the encoder in the reverse fashion to create the decoder
-    Dropout(0.2),
-    TimeDistributed(Dense(num_features))  # add time distributed layer to get output in correct shape.
-    # creates a vector of length = num features output from previous layer.
-    # (128) output, creates 128 vector and duplicates the number of features . (in this case, 1)
-])
 
-model.compile(loss='mae', optimizer='adam')
+def create_model(cells, time_steps, num_features, dropout, input_loss='mae', input_optimizer='adam'):
+    """Uses sequential model class from keras. Adds LSTM layer. Input samples, timesteps, features.
+    Hyperparameters include number of cells, dropout rate. Output is encoded feature vector of the input data.
+    Uses autoencoder by mirroring/reversing encoder to be a decoder."""
+    model = Sequential([
+        LSTM(cells, input_shape=(time_steps, num_features)),  # one LSTM layer
+        Dropout(dropout),  # dropout regularization
+        RepeatVector(time_steps),  # replicates the feature vectors from LSTM layer output vector by the number of time steps (e.g., 30 times)
+        LSTM(cells, return_sequences=True),  # mirror the encoder in the reverse fashion to create the decoder
+        Dropout(dropout),
+        TimeDistributed(Dense(num_features))  # add time distributed layer to get output in correct shape.
+        # creates a vector of length = num features output from previous layer.
+        # (128) output, creates 128 vector and duplicates the number of features. (in this case, 1)
+        ])
+    print(model.optimizer)
+    model.compile(loss=input_loss, optimizer=input_optimizer)
+
+    return model
+
+
+model = create_model(10, timesteps, num_features, 0.2)
 model.summary()
 
 ## Task 6: Train the Autoencoder
@@ -115,15 +169,24 @@ model.summary()
 # early stopping to ensure that too many epochs are not used.
 # monitor the validation loss for improvements. If network doesn't improve for 3 consecutive epochs, stops training.
 
-es = tf.keras.callbacks.EarlyStopping(monitor='val_loss', patience=3, mode='min')
-history = model.fit(
-    X_train, y_train,
-    epochs=100,  # just set to something high, early stopping will monitor.
-    batch_size=32,  # this can be optimized later
-    validation_split=0.1,  # use 10% of data for validation, use 90% for training.
-    callbacks=[es],  # early stopping similar to earlier
-    shuffle=False   # because order matters
-)
+
+def train_model(X_train, y_train, patience, monitor='val_loss', mode='min', epochs=100, batch_size=32, validation_split=0.1):
+    """Fits the model to training data. Early stopping ensures that too many epochs of training are not used.
+    Monitors the validation loss for improvements.  """
+    es = tf.keras.callbacks.EarlyStopping(monitor=monitor, patience=patience, mode=mode)
+    history = model.fit(
+        X_train, y_train,
+        epochs=epochs,  # just set to something high, early stopping will monitor.
+        batch_size=batch_size,  # this can be optimized later
+        validation_split=validation_split,  # use 10% of data for validation, use 90% for training.
+        callbacks=[es],  # early stopping similar to earlier
+        shuffle=False   # because order matters
+    )
+
+    return history
+
+
+history = train_model(X_train, y_train, patience=3)
 
 ## Task 7: Plot Metrics and Evaluate the Model
 
@@ -139,37 +202,41 @@ plt.legend();
 # get model predictions on training data and determine mean absolute error
 # look at the loss for each individual point
 
-X_train_pred = model.predict(X_train)
+def evaluate_model(X_train, X_test):
+    """Gets model predictions on training data and determines mean absolute error. Evaluates model on test data."""
+    X_train_pred = model.predict(X_train)
+    train_mae_loss = pd.DataFrame(np.mean(np.abo(X_train_pred-X_train), axis=1), columns=['Error'])
+    model_eval = model.evaluate(X_test, y_test)
+    X_test_pred = model.predict(X_test)
+    test_mae_loss = np.mean(np.abs(X_test_pred - X_test), axis=1)
 
-train_mae_loss = pd.DataFrame(np.mean(np.abs(X_train_pred - X_train), axis=1), columns=['Error'])
-
-# evaluate model on test data
-# shows us the total loss of the model
-
-model.evaluate(X_test, y_test)
+    return X_train_pred, train_mae_loss, model_eval, X_test_pred, test_mae_loss
 
 
 # look at the distribution of the errors using a distribution plot
-sns.distplot(train_mae_loss, bins=50, kde=True);
-
+sns.distplot(train_mae_loss, bins=50, kde=True)
 # choose a threshold to use for anomalies based on x-axis. try where error is greater than 0.75, it's anomalous.
 threshold = 0.75
-X_test_pred = model.predict(X_test)
-# calculate mae on test data
-test_mae_loss = np.mean(np.abs(X_test_pred - X_test), axis=1)
-
 sns.distplot(test_mae_loss, bins=50, kde=True);
 
-## Task 8: Detect Anomalies
 
-THRESHOLD = 0.75
+def detect_anomalies(test, test_mae_loss, threshold):
+    """Examine distribution of model errors to select threshold for anomalies.
+    Add columns fro loss value, threshold, anomalous T/F.
+    Creates data fram of anomalies to explore with more granularity."""
+    test_score_df = pd.DataFrame(test[time_steps:])
+    # add additional columns for loss value, threshold, whether entry is anomaly or not. could set a variable threshold.
+    test_score_df['loss'] = test_mae_loss
+    test_score_df['threshold'] = threshold
+    test_score_df['anomaly'] = test_score_df.loss > test_score_df.threshold
+    test_score_df['cond'] = test[time_steps:].cond
+    anomalies = test_score_df[test_score_df.anomaly == True]
 
-test_score_df = pd.DataFrame(test[time_steps:])
-# add additional columns for loss value, threshold, whether entry is anomaly or not. could set a variable threshold.
-test_score_df['loss'] = test_mae_loss
-test_score_df['threshold'] = THRESHOLD
-test_score_df['anomaly'] = test_score_df.loss > test_score_df.threshold
-test_score_df['cond'] = test[time_steps:].cond
+    return test_score_df, anomalies
+
+
+
+
 
 fig = go.Figure()
 fig.add_trace(go.Scatter(x=test[time_steps:].datetime, y=test_score_df.loss,
@@ -183,7 +250,7 @@ fig.show()
 
 
 # create data frame for anomalies to explore with more granularity by subsetting
-anomalies = test_score_df[test_score_df.anomaly == True]
+
 anomalies.head()
 
 
