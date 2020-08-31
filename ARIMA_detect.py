fff67b92e044bcd912a0048121bfc4f5dcfc732f
Amber <amber.jones@usu.edu>
2020-08-31 09:28:58 -0600

    Ensuring that ARIMA and LSTM scripts work with refactored utilities scripts.

--- ARIMA_detect.py
+++ ARIMA_detect.py
@@ -10,11 +10,6 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import statsmodels.api as api
-from statsmodels.graphics.tsaplots import plot_acf, plot_pacf
-
-from pandas.plotting import register_matplotlib_converters
-# register_matplotlib_converters()
-# plt.rcParams.update({'figure.figsize':(9,7), 'figure.dpi':120})
 
 
 def build_arima_model(data, p, d, q, summary):
@@ -82,7 +77,7 @@ site = "Mendon"
 # site = "TonyGrove"
 # site = "WaterLab"
 # sensor = "temp"
-sensor = "cond"
+sensor = ['cond']
 # sensor = "ph"
 # sensor = "do"
 # sensor = "turb"
@@ -123,7 +118,8 @@ print("q: "+str(q))
 # EXECUTE FUNCTIONS #
 #########################################
 # Get data
-df_full, df = anomaly_utilities.get_data(site, sensor, year, path="/Users/amber/PycharmProjects/LRO-anomaly-detection/LRO_data/")
+df_full, sensor_array = anomaly_utilities.get_data(site, sensor, year, path="/Users/amber/PycharmProjects/LRO-anomaly-detection/LRO_data/")
+df = sensor_array['cond']
 
 # Run model
 model_fit, residuals, predictions = build_arima_model(df['raw'], p, d, q, summary=True)
@@ -138,25 +134,24 @@ df['detected_anomaly'] = detected_anomaly[0]
 df['detected_event'] = anomaly_utilities.anomaly_events(detected_anomaly[0])
 
 # Determine Metrics
-labeled_in_detected, detected_in_labeled, valid_detections, invalid_detections = anomaly_utilities.compare_labeled_detected(df)
-TruePositives, FalseNegatives, FalsePositives, TrueNegatives, PRC, PPV, NPV, ACC, RCL, f1, f2 \
-    = anomaly_utilities.metrics(df, valid_detections, invalid_detections)
+compare = anomaly_utilities.compare_labeled_detected(df)
+metrics = anomaly_utilities.metrics(df, compare.valid_detections, compare.invalid_detections)
 
 # OUTPUT RESULTS #
 #########################################
 print('\n\n\nScript report:\n')
-print('Sensor: ' + sensor)
+print('Sensor: ' + sensor[0])
 print('Year: ' + str(year))
 print('Parameters: ARIMA(%i, %i, %i), Threshold = %f' %(p, d, q, threshold))
-print('PPV = %f' % PPV)
-print('NPV = %f' % NPV)
-print('Acc = %f' % ACC)
-print('TP  = %i' % TruePositives)
-print('TN  = %i' % TrueNegatives)
-print('FP  = %i' % FalsePositives)
-print('FN  = %i' % FalseNegatives)
-print('F1 = %f' % f1)
-print('F2 = %f' % f2)
+print('PPV = %f' % metrics.PPV)
+print('NPV = %f' % metrics.NPV)
+print('Acc = %f' % metrics.ACC)
+print('TP  = %i' % metrics.TruePositives)
+print('TN  = %i' % metrics.TrueNegatives)
+print('FP  = %i' % metrics.FalsePositives)
+print('FN  = %i' % metrics.FalseNegatives)
+print('F1 = %f' % metrics.f1)
+print('F2 = %f' % metrics.f2)
 print("\nTime Series ARIMA script end.")
 
 
@@ -171,61 +166,3 @@ plt.legend()
 plt.ylabel(sensor)
 plt.show()
 
-
-# EXPLORING SEASONAL ARIMA. Seems like seasonal would be useful given the diel fluctuation of ambient/aquatic variables.
-# However, using 96 as the seasonal basis (4x24 hours) is computationally intensive.
-# In parameter searching, seasonal variables were not significant?
-
-# fig = api.graphics.tsa.plot_acf(srs.diff().dropna(), lags=700)
-# plt.show()
-# d = pm.arima.ndiffs(srs, test='adf')
-# D = pm.arima.nsdiffs(srs, m=96, max_D=5)
-# print("D = " + str(D)+"\nd = "+str(d)+"\n")
-# model = pm.auto_arima(srs, start_p=1, start_q=1,
-#     # test='adf', # use adftest to find optimal 'd'
-#     max_p=3,
-#     max_q=3,
-#     seasonal=False,
-#     # seasonal=True, # daily seasonality
-#     # m=96, # number of samples per seasonal cycle, 4 samples/hr * 24hrs
-#     d=0,
-#     start_P=0,
-#     # D=D,
-#     trace=True,
-#     error_action='ignore',
-#     suppress_warnings=True,
-#     stepwise=True)
-# print(model.summary())
-
-# initial exploration into parameter grid search
-# p_vals = [0,1,2,3,4]
-# d_vals = [0,1]
-# q_vals = [0,1,2,3,4]
-# for p in p_vals:
-#   for d in d_vals:
-#     for q in q_vals:
-#       try:
-#         order = (p,d,q)
-#         model = ARIMA(srs, order)
-#         model_fit = model.fit(disp=0)
-#         print(model_fit)
-
-# build ARIMA model
-
-
-# Trying to use f score to maximize threshold
-# def find_threshold(min, max, inc, normal_lbl, anomDetns):
-#     f_score = []
-#     thresholds = np.arange(min, max, inc) #set range and increments for threshold. will need to
-#     generalize for other variables.
-#     for threshold in thresholds:
-#
-#
-#         f1 = metrics.f1_score(normal_lbl, anomDetns)
-#         f_score.append(f1)
-#
-#     sns.lineplot(thresholds, f_score)
-#     plt.xlabel("threshold")
-#     plot.ylabel("f1 score")
-#     opt_threshold =
-#     return (thresholds, f_score, opt_threshold)
