944f3e8b516ae91a80dd6f3e4045c214028f277f
Amber <amber.jones@usu.edu>
2020-10-02 13:14:30 -0600

    resolving minor issues with dynamic threhsold detection. moving arima functions to anomaly utilities script

--- anomaly_utilities.py
+++ anomaly_utilities.py
@@ -10,7 +10,8 @@
 import os
 import numpy as np
 import pandas as pd
-from scipy.stats import norm, t
+from scipy.stats import norm
+import statsmodels.api as api
 pd.options.mode.chained_assignment = None
 
 
@@ -225,16 +226,17 @@ def xfade(xfor, xbac):
 
         # add the results
         x = xfor_faded + xbac_faded
+
     return x
 
 
 def set_dynamic_threshold(residuals, alpha, window_sz):
     """Determines a threshold based on the local confidence interval, 
-    considering the data looking forward and backward window_sz steps.
-    data is a series like object
+    considering the model residuals looking forward and backward window_sz steps.
+    residuals is a series like object or a data frame
     alpha is a scalar between 0 and 1 representing the acceptable uncertainty
     window_sz is an integer representing how many data points to use in both directions
-    the return value is an array of pairs
+    the return value is a data frame of pairs
     """
     threshold = []  # initialize empty list to hold thresholds
     z = norm.ppf(1 - alpha / 2)
@@ -264,3 +266,67 @@ def set_dynamic_threshold(residuals, alpha, window_sz):
     threshold = pd.DataFrame(threshold, columns=['low', 'high'])
 
     return threshold
+
+
+def set_cons_threshold(model_fit, alpha_in):
+    """Determines threshold for anomaly detection based on confidence interval and specified alpha value using
+    SARIMAX model predict object."""
+    predict = model_fit.get_prediction()
+    predict_ci = predict.conf_int(alpha=alpha_in)
+    predict_ci.columns = ["lower", "upper"]
+    predict_ci["lower"][0] = predict_ci["lower"][1]
+
+    # This gives a constant interval for all points.
+    # Could also try a threshold to maximize F2, but that requires having labeled data. Could base on a portion of data?
+    thresholds = predict[0] - predict_ci["lower"]
+    threshold = thresholds[-1]
+
+    return threshold
+
+
+def detect_anomalies(residuals, threshold, summary=True):
+    """Compares residuals to threshold to identify anomalies. Can use set threshold level or threshold
+    determined by set_threshold function."""
+    # DETERMINE ANOMALIES
+    detected_anomaly = np.abs(residuals) > 11  # gives bools
+    detected_anomaly[0][0] = False  # set 1st value to false
+    # output summary
+    if summary:
+        print('\n\n')
+        print('\nratio of detections: %f' % ((sum(detected_anomaly[0])/len(detected_anomaly))*100), '%')
+
+    return detected_anomaly
+
+
+def detect_dyn_anomalies(residuals, threshold, summary=True):
+    """Compares residuals to threshold to identify anomalies. Can use set threshold level or threshold
+    determined by set_threshold function."""
+    # DETERMINE ANOMALIES
+    detected_anomaly = (residuals[0] < threshold['low']) | (threshold['high'] < residuals[0])  # gives bools
+    # output summary
+    if summary:
+        print('\n\n')
+        print('\nratio of detections: %f' % ((sum(detected_anomaly)/len(detected_anomaly))*100), '%')
+
+    return detected_anomaly
+
+
+def build_arima_model(data, p, d, q, summary):
+    """Builds an ARIMA model."""
+    model = api.tsa.SARIMAX(data, order=(p, d, q))
+    model_fit = model.fit(disp=0)
+    residuals = pd.DataFrame(model_fit.resid)
+    predict = model_fit.get_prediction()
+    predictions = pd.DataFrame(predict.predicted_mean)
+    residuals[0][0] = 0
+    predictions[0][0] = data[0]
+
+    # output summary
+    if summary:
+        print('\n\n')
+        print(model_fit.summary())
+        print('\n\nresiduals description:')
+        print(residuals.describe())
+
+    return model_fit, residuals, predictions
+
