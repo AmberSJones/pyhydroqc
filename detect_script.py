f5a063c145a3e4e9f66d2ae85375ab6f689b28e9
tannerlex <61562870+tannerlex@users.noreply.github.com>
2020-10-27 09:54:01 -0600

    refactored detect script to take parameters such that all data processing can be looped through (#64)

--- detect_script.py
+++ detect_script.py
@@ -8,123 +8,351 @@
 import anomaly_utilities
 import model_workflow
 import rules_detect
+import copy
 import pandas as pd
 
-#####################################
-# SITE SPECIFIC SETTINGS #
-#####################################
+site = 'BlackSmithFork'
 
-# RETRIEVE DATA #
+class WFParams:
+    pass
+    """
+    """
+
+
+wfp_data = []
+wfparam = WFParams()
+site_params = []
+
+# LSTM PARAMETERS #
 #########################################
-site = ' '
-sensor = ['temp', 'cond', 'ph', 'do']
-year = [2014, 2015, 2016, 2017, 2018, 2019]
-df_full, sensor_array = anomaly_utilities.get_data(site, sensor, year, path="./LRO_data/")
+wfparam.time_steps = 10
+wfparam.samples = 20000
+wfparam.cells = 128
+wfparam.dropout = 0.2
+wfparam.patience = 6
+
+
+############################
+# SITE SPECIFIC Parameters #
+############################
 
 # FRANKLIN BASIN PARAMETERS #
 #########################################
-maximum = [13, 380, 9.2, 13]
-minimum = [-2, 120, 7.5, 8]
-length = [6, 3, 3, 3]
-window_sz = [30, 30, 30, 30]
-alpha = [0.0001, 0.0001, 0.00001, 0.0001]
-min_range = [0.25, 4, 0.02, 0.15]
-wf = [1, 1, 1, 1]
+sensor_params = []
+#temp params
+wfparam.max_range = 13
+wfparam.min_range = -2
+wfparam.persist = 6
+wfparam.window_sz = 30
+wfparam.alpha = 0.0001
+wfparam.threshold_min = 0.25
+wfparam.widen = 1
+wfparam.pdq = [6, 1, 4]
+sensor_params.append(copy.deepcopy(wfparam))
+
+#cond params
+wfparam.max_range = 380
+wfparam.min_range = 120
+wfparam.persist = 3
+wfparam.window_sz = 30
+wfparam.alpha = 0.0001
+wfparam.threshold_min = 4.0
+wfparam.widen = 1
+wfparam.pdq = [10, 1, 3]
+sensor_params.append(copy.deepcopy(wfparam))
+
+#ph params
+wfparam.max_range = 9.2
+wfparam.min_range = 7.5
+wfparam.persist = 3
+wfparam.window_sz = 30
+wfparam.alpha = 0.00001
+wfparam.threshold_min = 0.02
+wfparam.widen = 1
+wfparam.pdq = [10, 1, 1]
+sensor_params.append(copy.deepcopy(wfparam))
+
+#do params
+wfparam.max_range = 13
+wfparam.min_range = 8
+wfparam.persist = 3
+wfparam.window_sz = 30
+wfparam.alpha = 0.0001
+wfparam.threshold_min = 0.15
+wfparam.widen = 1
+wfparam.pdq = [0, 1, 5]
+sensor_params.append(copy.deepcopy(wfparam))
+
+site_params.append(copy.deepcopy(copy.deepcopy(sensor_params)))
 
 # TONY GROVE PARAMETERS #
 #########################################
-maximum = [20, 500, 9.0, 14]
-minimum = [-2, 175, 8.0, 7]
-length = 6
-window_sz = [30, 40, 40, 30]
-alpha = [0.00001, 0.00001, 0.00001, 0.0001]
-min_range = [0.4, 5, 0.02, 0.15]
-wf = [1, 1, 1, 1]
+sensor_params = []
+#temp params
+wfparam.max_range = 20
+wfparam.min_range = -2
+wfparam.persist = 6
+wfparam.window_sz = 30
+wfparam.alpha = 0.00001
+wfparam.threshold_min = 0.4
+wfparam.widen = 1
+wfparam.pdq = [10, 1, 0]
+sensor_params.append(copy.deepcopy(wfparam))
+
+#cond params
+wfparam.max_range = 500
+wfparam.min_range = 175
+wfparam.persist = 3
+wfparam.window_sz = 40
+wfparam.alpha = 0.00001
+wfparam.threshold_min = 5.0
+wfparam.widen = 1
+wfparam.pdq = [6, 1, 2]
+sensor_params.append(copy.deepcopy(wfparam))
+
+#ph params
+wfparam.max_range = 9.0
+wfparam.min_range = 8.0
+wfparam.persist = 3
+wfparam.window_sz = 40
+wfparam.alpha = 0.00001
+wfparam.threshold_min = 0.02
+wfparam.widen = 1
+wfparam.pdq = [8, 1, 4]
+sensor_params.append(copy.deepcopy(wfparam))
+
+#do params
+wfparam.max_range = 14
+wfparam.min_range = 7
+wfparam.persist = 3
+wfparam.window_sz = 30
+wfparam.alpha = 0.0001
+wfparam.threshold_min = 0.15
+wfparam.widen = 1
+wfparam.pdq = [10, 1, 0]
+sensor_params.append(copy.deepcopy(wfparam))
+
+site_params.append(copy.deepcopy(copy.deepcopy(sensor_params)))
 
 # WATERLAB PARAMETERS #
 #########################################
-maximum = [18, 450, 9.2, 14]
-minimum = [-2, 200, 8.0, 7]
-length = 6
-window_sz = [30, 40, 40, 30]
-alpha = [0.0001, 0.0001, 0.00001, 0.00001]
-min_range = [0.4, 5, 0.02, 0.15]
-wf = [1, 1, 1, 1]
+sensor_params = []
+#temp params
+wfparam.max_range = 18
+wfparam.min_range = -2
+wfparam.persist = 6
+wfparam.window_sz = 30
+wfparam.alpha = 0.0001
+wfparam.threshold_min = 0.4
+wfparam.widen = 1
+wfparam.pdq = [0, 1, 5]
+sensor_params.append(copy.deepcopy(wfparam))
+
+#cond params
+wfparam.max_range = 450
+wfparam.min_range = 200
+wfparam.persist = 3
+wfparam.window_sz = 40
+wfparam.alpha = 0.0001
+wfparam.threshold_min = 5.0
+wfparam.widen = 1
+wfparam.pdq = [7, 1, 0]
+sensor_params.append(copy.deepcopy(wfparam))
+
+#ph params
+wfparam.max_range = 9.2
+wfparam.min_range = 8.0
+wfparam.persist = 3
+wfparam.window_sz = 40
+wfparam.alpha = 0.00001
+wfparam.threshold_min = 0.02
+wfparam.widen = 1
+wfparam.pdq = [10, 1, 0]
+sensor_params.append(copy.deepcopy(wfparam))
+
+#do params
+wfparam.max_range = 14
+wfparam.min_range = 7
+wfparam.persist = 3
+wfparam.window_sz = 30
+wfparam.alpha = 0.00001
+wfparam.threshold_min = 0.15
+wfparam.widen = 1
+wfparam.pdq = [1, 1, 1]
+sensor_params.append(copy.deepcopy(wfparam))
+
+site_params.append(copy.deepcopy(copy.deepcopy(sensor_params)))
 
 # MAIN STREET PARAMETERS #
 #########################################
-maximum = [20, 2700, 9.5, 15]
-minimum = [-2, 150, 7.5, 5]
-length = 6
-window_sz = [30, 40, 20, 30]
-alpha = [0.00001, 0.000001, 0.0001, 0.00001]
-min_range = [0.4, 5, 0.03, 0.25]
-wf = [1, 1, 1, 1]
+sensor_params = []
+#temp params
+wfparam.max_range = 20
+wfparam.min_range = -2
+wfparam.persist = 6
+wfparam.window_sz = 30
+wfparam.alpha = 0.00001
+wfparam.threshold_min = 0.4
+wfparam.widen = 1
+wfparam.pdq = [0, 0, 0]
+sensor_params.append(copy.deepcopy(wfparam))
+
+#cond params
+wfparam.max_range = 2700
+wfparam.min_range = 150
+wfparam.persist = 3
+wfparam.window_sz = 40
+wfparam.alpha = 0.000001
+wfparam.threshold_min = 5.0
+wfparam.widen = 1
+wfparam.pdq = [1, 1, 5]
+sensor_params.append(copy.deepcopy(wfparam))
+
+#ph params
+wfparam.max_range = 9.5
+wfparam.min_range = 7.5
+wfparam.persist = 3
+wfparam.window_sz = 20
+wfparam.alpha = 0.0001
+wfparam.threshold_min = 0.03
+wfparam.widen = 1
+wfparam.pdq = [3, 1, 1]
+sensor_params.append(copy.deepcopy(wfparam))
+
+#do params
+wfparam.max_range = 15
+wfparam.min_range = 5
+wfparam.persist = 3
+wfparam.window_sz = 30
+wfparam.alpha = 0.00001
+wfparam.threshold_min = 0.25
+wfparam.widen = 1
+wfparam.pdq = [1, 1, 1]
+sensor_params.append(copy.deepcopy(wfparam))
+
+site_params.append(copy.deepcopy(copy.deepcopy(sensor_params)))
 
 # MENDON PARAMETERS #
 #########################################
-maximum = [28, 800, 9.0, 15]
-minimum = [-2, 200, 7.4, 3]
-length = 6
-window_sz = [30, 40, 20, 30]
-alpha = [0.0001, 0.00001, 0.0001, 0.001]
-min_range = [0.4, 5, 0.03, 0.15]
-wf = [1, 1, 1, 1]
+sensor_params = []
+#temp params
+wfparam.max_range = 28
+wfparam.min_range = -2
+wfparam.persist = 6
+wfparam.window_sz = 30
+wfparam.alpha = 0.0001
+wfparam.threshold_min = 0.4
+wfparam.widen = 1
+wfparam.pdq = [3, 1, 1]
+sensor_params.append(copy.deepcopy(wfparam))
+
+#cond params
+wfparam.max_range = 800
+wfparam.min_range = 200
+wfparam.persist = 3
+wfparam.window_sz = 40
+wfparam.alpha = 0.00001
+wfparam.threshold_min = 5.0
+wfparam.widen = 1
+wfparam.pdq = [9, 1, 4]
+sensor_params.append(copy.deepcopy(wfparam))
+
+#ph params
+wfparam.max_range = 9.0
+wfparam.min_range = 7.4
+wfparam.persist = 3
+wfparam.window_sz = 20
+wfparam.alpha = 0.0001
+wfparam.threshold_min = 0.03
+wfparam.widen = 1
+wfparam.pdq = [0, 1, 2]
+sensor_params.append(copy.deepcopy(wfparam))
+
+#do params
+wfparam.max_range = 15
+wfparam.min_range = 3
+wfparam.persist = 3
+wfparam.window_sz = 30
+wfparam.alpha = 0.001
+wfparam.threshold_min = 0.15
+wfparam.widen = 1
+wfparam.pdq = [10, 1, 3]
+sensor_params.append(copy.deepcopy(wfparam))
+
+site_params.append(copy.deepcopy(copy.deepcopy(sensor_params)))
 
 # BLACKSMITH FORK PARAMETERS #
 #########################################
-maximum = [28, 900, 9.2, 14]
-minimum = [-2, 200, 7.2, 2]
-length = 6
-window_sz = [30, 20, 30, 30]
-alpha = [0.0001, 0.01, 0.00001, 0.0001]
-min_range = [0.4, 4, 0.03, 0.15]
-wf = [1, 1, 1, 1]
+sensor_params = []
+#temp params
+wfparam.max_range = 28
+wfparam.min_range = -2
+wfparam.persist = 6
+wfparam.window_sz = 30
+wfparam.alpha = 0.0001
+wfparam.threshold_min = 0.4
+wfparam.widen = 1
+wfparam.pdq = [1, 1, 0]
+sensor_params.append(copy.deepcopy(wfparam))
+
+#cond params
+wfparam.max_range = 900
+wfparam.min_range = 200
+wfparam.persist = 3
+wfparam.window_sz = 20
+wfparam.alpha = 0.01
+wfparam.threshold_min = 4.0
+wfparam.widen = 1
+wfparam.pdq = [0, 0, 5]
+sensor_params.append(copy.deepcopy(wfparam))
+
+#ph params
+wfparam.max_range = 9.2
+wfparam.min_range = 7.2
+wfparam.persist = 3
+wfparam.window_sz = 30
+wfparam.alpha = 0.00001
+wfparam.threshold_min = 0.03
+wfparam.widen = 1
+wfparam.pdq = [0, 1, 4]
+sensor_params.append(copy.deepcopy(wfparam))
+
+#do params
+wfparam.max_range = 14
+wfparam.min_range = 2
+wfparam.persist = 3
+wfparam.window_sz = 30
+wfparam.alpha = 0.0001
+wfparam.threshold_min = 0.15
+wfparam.widen = 1
+wfparam.pdq = [0, 0, 5]
+sensor_params.append(copy.deepcopy(wfparam))
+
+site_params.append(copy.deepcopy(copy.deepcopy(sensor_params)))
+
+
+# RETRIEVE DATA #
+#########################################
+sites = ['FranklinBasin', 'TonyGrove', 'WaterLab', 'MainStreet', 'Mendon', 'BlackSmithFork']
+year = [2014, 2015, 2016, 2017, 2018, 2019]
+sensor = ['temp', 'cond', 'ph', 'do']
+if site == 'BlackSmithFork': year.pop(0)
+
+j = sites.index(site)
+df_full, sensor_array = anomaly_utilities.get_data(sites[j], sensor, year, path="./LRO_data/")
+
 
 # RULES BASED ANOMALY DETECTION #
 #########################################
 size = []
 for i in range(0, len(sensor_array)):
-    sensor_array[sensor[i]] = rules_detect.range_check(sensor_array[sensor[i]], maximum[i], minimum[i])
-    sensor_array[sensor[i]] = rules_detect.persistence(sensor_array[sensor[i]], length[i])
+    sensor_array[sensor[i]] = rules_detect.range_check(sensor_array[sensor[i]], site_params[j][i].max_range, site_params[j][i].min_range)
+    sensor_array[sensor[i]] = rules_detect.persistence(sensor_array[sensor[i]], site_params[j][i].persist)
     s = rules_detect.group_size(sensor_array[sensor[i]])
     size.append(s)
     sensor_array[sensor[i]] = rules_detect.interpolate(sensor_array[sensor[i]])
-    print(str(sensor[i]) + ' maximum detected group length = ' + str(size[i]))
+    print(str(sensor[i]) + ' longest detected group = ' + str(size[i]))
 print('Rules based detection complete.\n')
-
-# ARIMA PARAMETERS #
-#########################################
-# Need to use an automated method to generalize getting p,d,q parameters
-# These are the results of using auto.ARIMA to determine p,d,q parameters in R
-sites = {'BlackSmithFork': 0,
-         'FranklinBasin': 1,
-         'MainStreet': 2,
-         'Mendon': 3,
-         'TonyGrove': 4,
-         'WaterLab': 5}
-sensors = {'cond': 0,
-           'do': 1,
-           'ph': 2,
-           'temp': 3,
-           'turb': 4}
-pdqParams = [
-    [[0, 0, 5], [0, 0, 5], [0, 1, 4], [1, 1, 0], [9, 1, 5]],  # BlackSmithFork
-    [[10, 1, 3], [0, 1, 5], [10, 1, 1], [6, 1, 4], [0, 1, 5]],  # FranklinBasin
-    [[1, 1, 5], [1, 1, 1], [3, 1, 1], [0, 0, 0], [1, 1, 5]],  # MainStreet
-    [[9, 1, 4], [10, 1, 3], [0, 1, 2], [3, 1, 1], [9, 1, 1]],  # Mendon
-    [[6, 1, 2], [10, 1, 0], [8, 1, 4], [10, 1, 0], [10, 1, 5]],  # TonyGrove
-    [[7, 1, 0], [1, 1, 1], [10, 1, 0], [0, 1, 5], [1, 1, 3]]  # WaterLab
-]
-pdqParam = pd.DataFrame(pdqParams, columns=sensors, index=sites)
-
-# LSTM PARAMETERS #
-#########################################
-time_steps = 10
-samples = 20000
-cells = 128
-dropout = 0.2
-patience = 6
 
 
 ##############################################
@@ -135,15 +363,11 @@ patience = 6
 #########################################
 ARIMA_detect = []
 for i in range(0, len(sensor)):
-    p, d, q = pdqParam[sensor[i]][site]
-    print('sensor: ' + str(sensor[i]) + ', p, d, q = (' + str(p) + ', ' + str(d) + ', ' + str(q) + ')')
     df = sensor_array[sensor[i]]
     ARIMA_detect.append(
         model_workflow.ARIMA_detect(
-            df, sensor[i], p, d, q,
-            minimum[i], maximum[i], length[i],
-            window_sz[i], alpha[i], min_range[i], wf[i],
-            rules=False, plots=False, save_figs=True, summary=False, output=True, site=site
+            df, sensor[i], site_params[j][i],
+            rules=False, plots=False, summary=False, output=True, site=site
             ))
 
 # LSTM BASED DETECTION #
@@ -156,11 +380,8 @@ for i in range(0, len(sensor)):
     df = sensor_array[sensor[i]]
     LSTM_detect_univar.append(
         model_workflow.LSTM_detect_univar(
-            df, sensor[i],
-            minimum[i], maximum[i], length[i],
-            model_type, time_steps, samples, cells, dropout, patience,
-            window_sz[i], alpha[i], min_range[i], wf[i],
-            rules=False, plots=False, save_figs=True, summary=False, output=True, site=site
+            df, sensor[i], site_params[j][i], model_type,
+            rules=False, plots=False, summary=False, output=True, site=site
         ))
 
 # DATA: univariate,  MODEL: bidirectional #
@@ -170,33 +391,24 @@ for i in range(0, len(sensor)):
     df = sensor_array[sensor[i]]
     LSTM_detect_univar_bidir.append(
         model_workflow.LSTM_detect_univar(
-            df, sensor[i],
-            minimum[i], maximum[i], length,
-            model_type, time_steps, samples, cells, dropout, patience,
-            window_sz[i], alpha[i], min_range[i], wf[i],
-            rules=False, plots=False, save_figs=True, summary=False, output=True, site=site
+            df, sensor[i], site_params[j][i], model_type,
+            rules=False, plots=False, summary=False, output=True, site=site
             ))
 
 # DATA: multivariate,  MODEL: vanilla #
 model_type = 'vanilla'
 LSTM_detect_multivar = \
     model_workflow.LSTM_detect_multivar(
-        sensor_array, sensor,
-        minimum, maximum, length,
-        model_type, time_steps, samples, cells, dropout, patience,
-        window_sz, alpha, min_range, wf,
-        rules=False, plots=False, save_figs=True, summary=False, output=True, site=site
+        sensor_array, sensor, site_params[j], model_type,
+        rules=False, plots=False, summary=False, output=True, site=site
         )
 
 # DATA: multivariate,  MODEL: bidirectional #
 model_type = 'bidirectional'
 LSTM_detect_multivar_bidirectional = \
     model_workflow.LSTM_detect_multivar(
-        sensor_array, sensor,
-        minimum, maximum, length,
-        model_type, time_steps, samples, cells, dropout, patience,
-        window_sz, alpha, min_range, wf,
-        rules=False, plots=False, save_figs=True, summary=False, output=True, site=site
+        sensor_array, sensor, site_params[j], model_type,
+        rules=False, plots=False, summary=False, output=True, site=site
         )
 
 #########################################
