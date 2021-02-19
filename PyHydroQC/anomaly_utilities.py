aa01cee437bb6dc2236f7d3279d7fbd7c2d3c672
Amber <amber.jones@usu.edu>
2021-02-18 22:34:49 -0700

    changes to make comparison to labels optional in the workflow

--- PyHydroQC/anomaly_utilities.py
+++ PyHydroQC/anomaly_utilities.py
@@ -449,11 +449,12 @@ def detect_dyn_anomalies(residuals, threshold, summary=True):
     return detected_anomaly
 
 
-def aggregate_results(df, results_ARIMA, results_LSTM_van_uni, results_LSTM_bidir_uni, results_LSTM_van_multi, results_LSTM_bidir_multi, compare=False):
+def aggregate_results(df, results_ARIMA, results_LSTM_van_uni, results_LSTM_bidir_uni, results_LSTM_van_multi, results_LSTM_bidir_multi, verbose=False, compare=False):
     """
     Each results input argument is a dataframe with the column 'detected_event'.
     """
-    results_all = pd.DataFrame(index = results_ARIMA.index)
+    results_all = pd.DataFrame(index=results_ARIMA.index)
+    results_all['observed'] = df['observed']
     results_all['ARIMA'] = results_ARIMA['detected_event'] > 0
     results_all['LSTM_van_uni'] = results_LSTM_van_uni['detected_event'] > 0
     results_all['LSTM_bidir_uni'] = results_LSTM_bidir_uni['detected_event'] > 0
@@ -461,6 +462,13 @@ def aggregate_results(df, results_ARIMA, results_LSTM_van_uni, results_LSTM_bidi
     results_all['LSTM_bidir_multi'] = results_LSTM_bidir_multi['detected_event'] > 0
 
     results_all['detected_event'] = results_all.eval('ARIMA or LSTM_van_uni or LSTM_bidir_uni or LSTM_van_multi or LSTM_bidir_multi')
+
+    if ~verbose:
+        results_all = results_all.drop('ARIMA', 1)
+        results_all = results_all.drop('LSTM_van_uni', 1)
+        results_all = results_all.drop('LSTM_bidir_uni', 1)
+        results_all = results_all.drop('LSTM_van_multi', 1)
+        results_all = results_all.drop('LSTM_bidir_multi', 1)
 
     if compare:
         results_all['labeled_anomaly'] = df['labeled_anomaly']
