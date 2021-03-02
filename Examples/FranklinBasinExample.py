394c0f3378b43927df666c89065317961e24aa9f
Amber <amber.jones@usu.edu>
2021-03-01 22:36:10 -0700

    specifying arguments in all functions for clarity

--- Examples/FranklinBasinExample.py
+++ Examples/FranklinBasinExample.py
@@ -18,7 +18,7 @@ from Examples.FB_parameters import site_params, LSTM_params, calib_params
 #########################################
 site = 'FranklinBasin'
 sensors = ['temp', 'cond', 'ph', 'do']
-sensor_array = anomaly_utilities.get_data(sensors, filename='FB2017.csv', path="LRO_data/")
+sensor_array = anomaly_utilities.get_data(sensors=sensors, filename='FB2017.csv', path="LRO_data/")
 
 #### Rules Based Anomaly Detection
 #########################################
@@ -26,11 +26,11 @@ range_count = dict()
 persist_count = dict()
 rules_metrics = dict()
 for snsr in sensor_array:
-    sensor_array[snsr], range_count[snsr] = \
-        rules_detect.range_check(sensor_array[snsr], site_params[site][snsr]['max_range'], site_params[site][snsr]['min_range'])
-    sensor_array[snsr], persist_count[snsr] = \
-        rules_detect.persistence(sensor_array[snsr], site_params[site][snsr]['persist'], output_grp=True)
-    sensor_array[snsr] = rules_detect.interpolate(sensor_array[snsr])
+    sensor_array[snsr], range_count[snsr] = rules_detect.range_check(
+        df=sensor_array[snsr], maximum=site_params[site][snsr]['max_range'], minimum=site_params[site][snsr]['min_range'])
+    sensor_array[snsr], persist_count[snsr] = rules_detect.persistence(
+        df=sensor_array[snsr], length=site_params[site][snsr]['persist'], output_grp=True)
+    sensor_array[snsr] = rules_detect.interpolate(df=sensor_array[snsr])
 print('Rules based detection complete.\n')
 
 #### Detect Calibration Events
@@ -39,8 +39,8 @@ calib_sensors = sensors[1:4]
 input_array = dict()
 for snsr in calib_sensors:
     input_array[snsr] = sensor_array[snsr]
-all_calib, all_calib_dates, df_all_calib, calib_dates_overlap = \
-    rules_detect.calib_overlap(calib_sensors, input_array, calib_params)
+all_calib, all_calib_dates, df_all_calib, calib_dates_overlap = rules_detect.calib_overlap(
+    sensor_names=calib_sensors, input_array=input_array, calib_params=calib_params)
 
 #### Model Based Anomaly Detection
 #########################################
@@ -49,13 +49,13 @@ all_calib, all_calib_dates, df_all_calib, calib_dates_overlap = \
 #########################################
 all_pdq = dict()
 for snsr in sensors:
-    all_pdq[snsr] = modeling_utilities.pdq(sensor_array[snsr]['observed'])
+    all_pdq[snsr] = modeling_utilities.pdq(data=sensor_array[snsr]['observed'])
     print(snsr + ' (p, d, q) = ' + str(all_pdq[snsr]))
     site_params[site][snsr]['pdq'] = all_pdq[snsr]
 
 ARIMA = dict()
 for snsr in sensors:
-    ARIMA[snsr] = model_workflow.ARIMA_detect(sensor_array[snsr], snsr, site_params[site][snsr],
+    ARIMA[snsr] = model_workflow.ARIMA_detect(df=sensor_array[snsr], sensor=snsr, params=site_params[site][snsr],
                                               rules=False, plots=False, summary=False, compare=False)
 print('ARIMA detection complete.\n')
 
@@ -66,7 +66,7 @@ print('ARIMA detection complete.\n')
 LSTM_univar = dict()
 for snsr in sensors:
     LSTM_univar[snsr] = model_workflow.LSTM_detect_univar(
-            sensor_array[snsr], snsr, site_params[site][snsr], LSTM_params, model_type=ModelType.VANILLA,
+            df=sensor_array[snsr], sensor=snsr, params=site_params[site][snsr], LSTM_params=LSTM_params, model_type=ModelType.VANILLA,
             rules=False, plots=False, summary=False, compare=False, model_output=False, model_save=False)
 
 ###### DATA: univariate,  MODEL: bidirectional
@@ -74,19 +74,19 @@ for snsr in sensors:
 LSTM_univar_bidir = dict()
 for snsr in sensors:
     LSTM_univar_bidir[snsr] = model_workflow.LSTM_detect_univar(
-            sensor_array[snsr], snsr, site_params[site][snsr], LSTM_params, model_type=ModelType.BIDIRECTIONAL,
+            df=sensor_array[snsr], sensor=snsr, params=site_params[site][snsr], LSTM_params=LSTM_params, model_type=ModelType.BIDIRECTIONAL,
             rules=False, plots=False, summary=False, compare=False, model_output=False, model_save=False)
 
 ###### DATA: multivariate,  MODEL: vanilla
 
 LSTM_multivar = model_workflow.LSTM_detect_multivar(
-        sensor_array, sensors, site_params[site], LSTM_params, model_type=ModelType.VANILLA,
+        sensor_array=sensor_array, sensors=sensors, params=site_params[site], LSTM_params=LSTM_params, model_type=ModelType.VANILLA,
         rules=False, plots=False, summary=False, compare=False, model_output=False, model_save=False)
 
 ###### DATA: multivariate,  MODEL: bidirectional
 
 LSTM_multivar_bidir = model_workflow.LSTM_detect_multivar(
-        sensor_array, sensors, site_params[site], LSTM_params, model_type=ModelType.BIDIRECTIONAL,
+        sensor_array=sensor_array, sensors=sensors, params=site_params[site], LSTM_params=LSTM_params, model_type=ModelType.BIDIRECTIONAL,
         rules=False, plots=False, summary=False, compare=False, model_output=False, model_save=False)
 
 ##### Aggregate Detections for All Models
@@ -100,7 +100,8 @@ for snsr in sensors:
     models['LSTM_univar_bidir'] = LSTM_univar_bidir[snsr].df_anomalies
     models['LSTM_multivar'] = LSTM_multivar.all_data[snsr]
     models['LSTM_multivar_bidir'] = LSTM_multivar_bidir.all_data[snsr]
-    results_all = anomaly_utilities.aggregate_results(sensor_array[snsr], models, verbose=True, compare=True)
+    results_all = anomaly_utilities.aggregate_results(
+        df=sensor_array[snsr], models=models, verbose=True, compare=True)
     aggregate_results[snsr] = results_all
 
 #### Correction
@@ -108,7 +109,8 @@ for snsr in sensors:
 
 corrections = dict()
 for snsr in sensors:
-    corrections[snsr] = ARIMA_correct.generate_corrections(aggregate_results[snsr], 'observed', 'detected_event')
+    corrections[snsr] = ARIMA_correct.generate_corrections(
+        df=aggregate_results[snsr], observed='observed', anomalies='detected_event')
 
 ############ PLOTTING ##############
 
