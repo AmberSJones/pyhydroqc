61266f925001d04bf77171dbce255e32b4f77d01
Amber <amber.jones@usu.edu>
2021-04-02 21:53:59 -0600

    adding warning options to correct.

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/AmberSJones/PyHydroQC",
-    packages=setuptools.find_packages(),
+    packages=setuptools.find_packages(include=['PyHydroQC']),
     classifiers=[
         "Programming Language :: Python :: 3",
         "Development Status :: 4 - Beta",
