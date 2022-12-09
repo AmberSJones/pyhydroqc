088ab032f3ade3accb70b787b860af1645d4befe
Maria Rivera Araya <50934176+mariariveraaraya@users.noreply.github.com>
2022-12-10 06:28:15 +1000

    trying mlflow (#1)

--- pyhydroqc/model_workflow.py
+++ pyhydroqc/model_workflow.py
@@ -287,6 +287,7 @@ def lstm_detect_multivar(sensor_array, sensors, params, LSTM_params, model_type,
             print('Event based calculations:')
             anomaly_utilities.print_metrics(e_metrics[snsr])
         print('Model report complete\n')
+            mlflow.log_metric("f2", e_metrics[snsr].f2)
 
     # GENERATE PLOTS #
     if plots:
