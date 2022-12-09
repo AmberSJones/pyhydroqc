3bbb06d89b52863df123d2f1cc497239f03d7e4f
Maria Rivera Araya <50934176+mariariveraaraya@users.noreply.github.com>
2022-12-10 09:25:38 +1000

    Update model_workflow.py

--- pyhydroqc/model_workflow.py
+++ pyhydroqc/model_workflow.py
@@ -45,12 +45,12 @@ def arima_detect(df, sensor, params,
     print('Threshold determination complete.')
     detections = anomaly_utilities.detect_anomalies(df['observed'], predictions, residuals, threshold, summary=True)
     with mlflow.start_run():
-    with mlflow.start_run(nested=True):
-        mlflow.log_param("p", p)
-    with mlflow.start_run(nested=True):
-        mlflow.log_param("p", p)
-    with mlflow.start_run(nested=True):
-        mlflow.log_param("p", p)
+        with mlflow.start_run(nested=True):
+            mlflow.log_param("p", p)
+        with mlflow.start_run(nested=True):
+            mlflow.log_param("p", p)
+        with mlflow.start_run(nested=True):
+            mlflow.log_param("p", p)
 
 
     # WIDEN AND NUMBER ANOMALOUS EVENTS #
